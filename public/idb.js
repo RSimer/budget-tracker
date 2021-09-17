658e6a72aff584fddbf68da5785d24f6f3312568
RSimer <65471088+RSimer@users.noreply.github.com>
2021-09-16 18:57:48 -0700

    init and final commit

---
+++ public/idb.js
@@ -0,0 +1,52 @@
+
+
+let db;
+const request = indexedDB.open('budget_tracker',1);
+request.onupgradeneeded = function(event){
+    const db = event.target.result;
+    db.createObjectStore('new_transaction',{autoIncrement: true});
+
+}
+request.onsuccess = function(event){
+    db = event.target.result;
+    if(navigator.onLine){
+        uploadTransaction();
+
+    }
+}
+request.onerror = function(event){
+    console.log(event.target.errorCode);
+}
+
+function saveRecord(record){
+    const transaction = db.transaction(['new_transaction'],'readwrite')
+    const transactionStore = transaction.objectStore('new_transaction')
+    transactionStore.add(record);
+}
+
+function uploadTransaction(){
+    const transaction = db.transaction(['new_transaction'],'readwrite')
+    const transactionStore = transaction.objectStore('new_transaction')
+    const getAll = transactionStore.getAll()
+    getAll.onsuccess = function(){
+        if(getAll.result.length > 0){
+            fetch('/api/transaction',{
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {Accept: 'application/json,text/plain,*/*','Content-Type':'Application/json'}
+            }).then(response => response.json())
+            .then(serverResponse =>{
+                if(serverResponse.message){
+                    throw new Error(serverResponse)
+                }
+                const transaction = db.transaction(['new_transaction'],'readwrite')
+                const transactionStore = transaction.objectStore('new_transaction')
+                transactionStore.clear();
+                alert('all transactions have been stored')
+            }).cache(err =>{
+                console.log(err)
+            })
+        }
+    }
+}
+window.addEventListener('online',uploadTransaction);
